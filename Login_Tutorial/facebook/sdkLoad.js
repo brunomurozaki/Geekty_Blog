20290f0dbccc23c878b27a5b8cb38c09e7edae38
bubui <brunoimurozaki@gmail.com>
2016-04-06 18:47:12 -0700

    Tirando o ID (J'a gerei outro, amiguinhos)

--- Login_Tutorial/facebook/sdkLoad.js
+++ Login_Tutorial/facebook/sdkLoad.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
 	  FB.init({
-		appId      : '1559579064358444',
+		appId      : 'XXXXXX',
 		cookie     : false,  // enable cookies to allow the server to access 
 							// the session
 		xfbml      : true,  // parse social plugins on this page
